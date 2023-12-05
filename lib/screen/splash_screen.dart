f84c2417734c7a97207d7e6cb39a7e1e45f7e11b
christingabrielsm123 <christingabriel@gmail.com>
2023-12-05 18:54:01 +0700

    register & splash screen

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, "/login");
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/shoes-1.png", width: 100, height: 100),
+            const CircularProgressIndicator()
+          ],
+        ),
+      ),
+    );
+  }
+}
